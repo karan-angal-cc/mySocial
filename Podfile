a87fea98684d3658b5056961cecb6e6c62d4377f
Karan Angal <karan_angal@hotmail.com>
2017-05-06 11:44:14 +0530

    Completed setting up firebase integration

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.2'
+
+target 'mySocial' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mySocial
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+
+
+
+
+end
---
+++ mySocial/signInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mySocial
+//
+//  Created by Karan Angal on 05/05/17.
+//  Copyright © 2017 KaranAngal. All rights reserved.
+//
+
+import UIKit
+
+class signInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
