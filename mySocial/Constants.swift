89086ffad5cfb219658c327e6259ae5cd66b9771
Karan Angal <karan_angal@hotmail.com>
2017-05-10 14:41:11 +0530

    Sign in UI custom classes complete

---
+++ mySocial/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  mySocial
+//
+//  Created by Karan Angal on 10/05/17.
+//  Copyright © 2017 KaranAngal. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
---
+++ mySocial/FancyBtn.swift
@@ -0,0 +1,23 @@
+//
+//  FancyBtn.swift
+//  mySocial
+//
+//  Created by Karan Angal on 10/05/17.
+//  Copyright © 2017 KaranAngal. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+
+    }
+}
